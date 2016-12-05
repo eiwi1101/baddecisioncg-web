8e6c07f8653698994737d0b869723d29779c23d1
eiwi1101 <weisert@eisertdev.com>
2016-12-05 16:04:28 -0600

    Login Page doesn't suck? Register doesn't work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root to: 'sessions#new'
 
+  # Session Handling
   get :login,  to: 'sessions#new'
   get :logout, to: 'sessions#destroy'
   post :login, to: 'sessions#create'
 
+  # Registration Handling
+  get :register, to: 'users#new'
+  post :register, to: 'users#create'
+
+  # Other Resources
   resources :game_lobbies, path: :play, only: [:index, :new, :show]
   resources :game_lobby_users, only: [:show]
-
-  if Rails.env.development?
-    get :styles, to: 'styles#index'
-  end
 end
