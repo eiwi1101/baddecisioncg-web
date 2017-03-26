93600ec1b830fa3494884200d03c66a972f969e7
William Eisert <weisert@eisertdev.com>
2017-03-25 20:03:44 -0500

    Rollbar. For error tracking. So we know what broke.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rollbar
+//
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
