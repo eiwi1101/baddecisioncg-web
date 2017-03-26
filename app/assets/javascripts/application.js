26fd11aea845d5b4a21b86e8b442da7f1d7ba808
William Eisert <weisert@eisertdev.com>
2017-03-25 21:23:28 -0500

    LOGIN SKREEN.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rollbar
+//= require '_rollbar'
 //
 //= require jquery
 //= require jquery.turbolinks
